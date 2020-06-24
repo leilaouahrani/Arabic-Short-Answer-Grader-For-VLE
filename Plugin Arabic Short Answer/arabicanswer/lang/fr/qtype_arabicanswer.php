60de6a2064f6d2259946bd931de1591d8819b879
Leila Ouahrani <66189111+leilaouahrani@users.noreply.github.com>
2020-06-24 21:24:52 +0200

    Update qtype_arabicanswer.php

--- Plugin Arabic Short Answer/arabicanswer/lang/fr/qtype_arabicanswer.php
+++ Plugin Arabic Short Answer/arabicanswer/lang/fr/qtype_arabicanswer.php
@@ -15,19 +15,19 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Defines the editing form for the arabicanswer question type.
+ * Defines the editing form for the arabicanswer1 question type.
  *
  * @package    qtype
- * @subpackage arabicanswer
- * @copyright  2019 Snoussi El Hareth & Madani Abderraouf
+ * @subpackage arabicanswer1
+ * @copyright  2019 Snoussi El Hareth & Madani Abderraouf For C00L07UN100120180002 Project
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$string['pluginname'] = 'Reponse Courte Semantique';
-$string['pluginname_help'] = 'Il suffit d\'entrer une réponse  modèle et la note sera automatiquement attribuée';
-$string['pluginname_link'] = 'question/type/arabicanswer';
-$string['pluginnameadding'] = 'Ajout d\'une question arabicanswer';
-$string['pluginnameediting'] = 'Modification d\'une question arabicanswer';
+$string['pluginname'] = 'Reponse Courte Semantique 1';
+$string['pluginname_help'] = 'Il faut entrer une réponse courte en ciblant directement le concept étudié';
+$string['pluginname_link'] = 'question/type/arabicanswer1';
+$string['pluginnameadding'] = 'Ajout d\'une question arabicanswer1';
+$string['pluginnameediting'] = 'Modification d\'une question arabicanswer1';
 $string['pluginnamesummary'] = 'Une question Reponse Courte Semantique est une question qui note  automatiquement les réponses des étudiants en fonction d’une similarité sémantique avec la réponse modèle de l’enseignant.';
 $string['test'] = 'test';
 $string['pleaseenterananswer'] = 'S\'il vous plaît entrer une réponse.';
